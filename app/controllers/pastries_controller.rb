aa0a0033c4aaf12cb69b4f95e1ed389bc8d4a6ef
Graham Chau <graham@childrensalon.com>
2015-03-05 12:18:26 +0000

    editing views, defining pastries controller

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,5 +1,15 @@
 class PastriesController < ApplicationController
   def index
+  	@pastries = [{
+  		name: "Croissant",
+  		price_in_pence: 350,
+  		description: " This traditional French pastry has a high real butter component, with lovely, light flaky layers of pastry"
+  		},
+  		{
+  			name: "Pain au Chocolat",
+  			price_in_pence: 380,
+  			description: "Pain au chocolat also called a chocolatine in the south-west of France (from Occitan chocolatina)"
+  			}]
   end
 
   def show
