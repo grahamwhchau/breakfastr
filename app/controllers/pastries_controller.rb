85d0e9b910e744a1d31b053d26ecfc9abf87a4d1
Graham Chau <graham@childrensalon.com>
2015-03-05 15:12:24 +0000

    generate controller

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -9,8 +9,44 @@ class PastriesController < ApplicationController
   end
 
   def new
+  	@pastry = Pastry.new
+  end
+
+  def create
+  	@pastry = Pastry.new(pastry_params)
+  	if @pastry.save
+  		flash[:success] = "Pastry added!"
+  		redirect_to root_path
+  	else
+  		flash[:error] = "Oops, try again"
+  		render :new
+  	end
   end
 
   def edit
+  	@pastry = Pastry.find(params[:id])
+  end
+
+  def update
+  	@pastry = Pastry.find(params[:id])
+  	if @pastry.update(pastry_params)
+  		flash[:success] ="Pastry updated"
+  		redirect_to pastry_path(@pastry)
+  	else
+  		flash[:error] = "Oops, try again."
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	pastry = Pastry.find(params[:id])
+  	pastry.destroy
+  	flash[:sucess] = "you've destroyed a perfectly good pastry."
+  	redirect_to root_path
+  end
+
+  private
+  def pastry_params
+  	params.require(:pastry).permit(:name, :price_in_pence, :description)
   end
 end
