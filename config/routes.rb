e5dddab1ada77158b278d26733693736677e1ee7
Graham Chau <graham@childrensalon.com>
2015-03-05 12:34:50 +0000

    define pastries routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'pastries/index'
-
-  get 'pastries/show'
-
-  get 'pastries/new'
-
-  get 'pastries/edit'
-
  root "pastries#index"
+ resources :pastries
 end
