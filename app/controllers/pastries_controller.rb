8b350404a25f4c3e718b172164b6c56351341d1d
Graham Chau <graham@childrensalon.com>
2015-03-05 12:42:50 +0000

    Generate pastry model

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -13,6 +13,11 @@ class PastriesController < ApplicationController
   end
 
   def show
+  	@pastry = {
+  		name: "Croissant",
+  		price_in_pence: 350,
+  		description: " This traditional French pastry has a high real butter component, with lovely, light flaky layers of pastry"
+  	}
   end
 
   def new
